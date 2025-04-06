f7a62a67c5088d116bfd933b3192287d2fa5b0d8
sa-es-ir <s.esmaeelinejad@gmail.com>
2025-04-06 19:27:57 +0100

    Add project structure and html for home page

---
+++ src/PersianAdminPanel.Application/IServiceCollectionExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PersianAdminPanel.Application;
+
+public static class IServiceCollectionExtension
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ src/PersianAdminPanel.Infrastructure/IServiceCollectionExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PersianAdminPanel.Infrastructure;
+
+public static class IServiceCollectionExtension
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
